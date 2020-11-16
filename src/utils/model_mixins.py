425dadea052038dbe6d78c32ccd01fc65f32ec43
erivanio <erivanio.vanin@gmail.com>
2020-11-16 15:35:18 -0300

    add created and modified in AnalysisMixin

--- src/utils/model_mixins.py
+++ src/utils/model_mixins.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class AnalysisMixin(models.Model):
-    start_date = models.DateField()
-    end_date = models.DateField()
-    data = models.JSONField(null=True, blank=True)
+    created = models.DateTimeField(_('created'), editable=False,
+                                   blank=True, auto_now_add=True)
+    modified = models.DateTimeField(_('modified'), editable=False,
+                                    blank=True, auto_now=True)
+    start_date = models.DateField(_('start date'))
+    end_date = models.DateField(_('end date'))
+    data = models.JSONField(_('data'), null=True, blank=True)
 
     class Meta:
         abstract = True
